afd7189927949361dd9c947a87b47945ac71d523
naga-kumar1084 <69434603+naga-kumar1084@users.noreply.github.com>
2020-08-09 14:25:36 -0500

    Add files via upload

---
+++ ConnectedCities/src/test/java/com/test/ConnectedCities/ConnectedCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ConnectedCities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectedCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
